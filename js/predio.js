09b959d748fca89846bd23a1ede2194373b513e8
Flávio Félix <fau_felix30@hotmail.com>
2025-05-13 15:25:33 -0300

    feat: creating floors, elevator attribute

---
+++ js/predio.js
@@ -0,0 +1,41 @@
+(function () {
+  // ----- Pavimentos
+
+  function criarTerreo() {
+    const janela = document.createElement("div");
+    janela.classList.add("janela");
+
+    const terreo = document.createElement("div");
+    terreo.classList.add("terreo");
+    terreo.appendChild(janela);
+    terreo.setAttribute("andar", "t");
+    return terreo;
+  }
+
+  function criarAndar(numero) {
+    const porta = document.createElement("div");
+    porta.classList.add("porta");
+
+    const andar = document.createElement("div");
+    andar.classList.add("andar");
+    andar.appendChild(porta);
+    andar.setAttribute("andar", numero);
+    return andar;
+  }
+
+  function criarPavimentos() {
+    const elementosComAndares = document.querySelectorAll("[andares]");
+
+    elementosComAndares.forEach((elComAndares) => {
+      const qtde = +elComAndares.getAttribute("andares");
+      for (let i = qtde; i > 0; i--) {
+        elComAndares.appendChild(criarAndar(i));
+      }
+      elComAndares.appendChild(criarTerreo());
+    });
+  }
+
+  criarPavimentos();
+
+  // ----- elevador
+})();
